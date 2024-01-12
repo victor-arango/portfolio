1cb5371fca555eeb420e06ef8bcd936549f38f8c
Victor Arango <victorarangoquiroz@gmail.com>
2024-01-12 11:36:20 -0500

    refactor model 3d

--- src/components/Model3d.jsx
+++ src/components/Model3d.jsx
@@ -5,3 +5,12 @@ export default function App() {
     <Spline scene="https://prod.spline.design/nYCbOsvI-8QvayIj/scene.splinecode" />
   );
 }
+
+
+// import Spline from '@splinetool/react-spline';
+
+// export default function App() {
+//   return (
+//     <Spline scene="https://prod.spline.design/qBT0Fj79lv4m99Du/scene.splinecode" />
+//   );
+// }
