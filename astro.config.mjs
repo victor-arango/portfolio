327d43ad1cad64a89da950ca4aa08722206ee729
Victor Arango <victorarangoquiroz@gmail.com>
2026-08-13 15:36:27 -0500

    resolve conflicts

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,10 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+    i18n:{
+        locales:["en", "esp"],
+        defaultLocale:"en",
+    },
     integrations:[
         react()],   
 },
